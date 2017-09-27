19e9f498e53fa2da4e4e5da7270e82f26a5cbf76
vagrant <vagrant@ubuntu-gnome-desktop-17.04-64>
2017-09-27 11:57:42 +0200

    make sh file executable

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,8 +2,9 @@
 sudo apt-get install libuv1-dev
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
-# git checkout e94b6e1
+git checkout e94b6e1
 mkdir build
+#export OPENSSL_ROOT_DIR=/usr/bin/openssl
 cd build
 cmake ..
 make 
